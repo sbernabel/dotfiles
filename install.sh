9c22b03d1a48124c5af83674d02417f2f184f94b
Silvano Bernabel <sbernabel@yahoo.com>
2024-07-30 23:39:48 -0400

    Added tmux config

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ ln -sf $DOTFILES_DIR/.bashrc $HOME/.bashrc
 ln -sf $DOTFILES_DIR/.bash_aliases $HOME/.bash_aliases
 ln -sf $DOTFILES_DIR/.config $HOME/.config
 ln -sf $DOTFILES_DIR/.tmux $HOME/.tmux
+ln -sf $DOTFILES_DIR/.tmux.conf $HOME/.tmux.conf
 
 echo "Dotfiles installed successfully!"
