c061efe7876c0b9b34aa761c5ce87eef6b3574ae
Silvano Bernabel <sbernabel@yahoo.com>
2024-07-31 00:03:52 -0400

    Added cargo files

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ DOTFILES_DIR="$HOME/dotfiles"
 ln -sf $DOTFILES_DIR/.bashrc $HOME/.bashrc
 ln -sf $DOTFILES_DIR/.bash_aliases $HOME/.bash_aliases
 ln -sf $DOTFILES_DIR/.config $HOME/.config
+ln -sf $DOTFILES_DIR/.cargo $HOME/.cargo
 ln -sf $DOTFILES_DIR/.tmux $HOME/.tmux
 ln -sf $DOTFILES_DIR/.tmux.conf $HOME/.tmux.conf
 
